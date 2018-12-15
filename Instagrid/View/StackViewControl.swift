4ed222469506d8c32ff67356f5b73f8ed9f209f7
Roland Lariotte <roland.sound@live.fr>
2018-12-15 14:31:40 +0100

    Finish the main design with stack views in the storyboard.

---
+++ Instagrid/View/StackViewControl.swift
@@ -0,0 +1,16 @@
+//
+//  StackViewControl.swift
+//  Instagrid
+//
+//  Created by Roland Lariotte on 14/12/2018.
+//  Copyright © 2018 Roland Lariotte. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class StackViewControl: UIViewController {
+    
+    
+}
